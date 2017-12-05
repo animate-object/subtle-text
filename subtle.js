a70a33aaa28843cf99dc65686b3a447bb105aab3
Alex Prinsen <alexander.f.prinsen@gmail.com>
2017-12-04 21:02:03 -0600

    tweaking/debugging random words

--- subtle.js
+++ subtle.js
@@ -1,14 +1,19 @@
 main = (manipulator) => {
     let elements = document.body.getElementsByTagName('*');
     for (const element of elements) {
-        if (["SCRIPT", "STYLE"].includes(element.tagName)) { continue }
-        for (const child of element.childNodes) {
-            if (child.nodeType === 3 && child.nodeValue !== null && child.nodeValue !== undefined) {
-                manipulated = manipulator(text);
-                if (manipulated !== null) {
-                    element.replaceChild(document.createTextNode(manipulated), child);
+        try {
+            if (["SCRIPT", "STYLE"].includes(element.tagName)) { continue }
+            for (const child of element.childNodes) {
+                if (child.nodeType === 3 && child.nodeValue !== null && child.nodeValue !== undefined) {
+                    manipulated = manipulator(child.nodeValue);
+                    if (manipulated !== null) {
+                        element.replaceChild(document.createTextNode(manipulated), child);
+                    }
                 }
             }
+        } catch (ex) {
+            console.log("failed to parse");
+            console.log(element);
         }
     }
 }
@@ -25,24 +30,34 @@ n_random_in_range = (n, range) => {
 
 
 weight_bounded_random = (n, weight) => {
-    // generates a random number between 0 and ceil(n/weight),
-    // s.t.
+    // generates a random number between 0 and ceil(n/weight)  s.t.
     // n = 1, weight = 100 has a 1 % chance to return 1, 
-    // n = 150, weight = 100 has a 25% chance to return 2, a 50% chance to return 1, and a 25% chance to return 0
+    // n = 101, weight = 100 has a 100% chance to return at least 1 and a 1% chance to return 2
     sum_randoms = 0;
-    for (i = 0; i < Math.ceil(n/weight); i++) { 
+    bound = n/weight
+    num_randoms = Math.ceil(n/weight);
+    count = 0;
+    for (i = 0; i < num_randoms; i++) { 
         sum_randoms = sum_randoms + Math.random();
+        if (sum_randoms < n/bound) { count = count + 1; }
     }
-    return Math.floor(sum_randoms);
+    return Math.floor(count);
 }
 
 
 random_word_additions = (text) => {
+    console.log("running random word additions");
     const words = text.split(" ");
     const addition_count = weight_bounded_random(words.length, 100);
     const addition_indices = n_random_in_range(addition_count, words.length);
-    copy = [];
+    if (addition_indices.length > 0) {
+        console.log(addition_indices);
+        console.log(text);
+    } else {
+        return null;
+    }
 
+    copy = [];
     for (i = 0; i < words.length; i++) {
         copy.push(words[i]);
         if (addition_indices.includes(i)) {
@@ -72,5 +87,5 @@ random_letter_additions = (text) => {
     return copy;
 }
 
-main(random_letter_additions);
+main(random_word_additions);
 
